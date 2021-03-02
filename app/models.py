aef60c7d7bf6d77fd61dd4e3747dec34e45223b0
jaysmit8340 <jasonsmit95@gmail.com>
2021-03-01 17:32:49 -0800

    Changes were made to the files

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class UserProfile(db.Model):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
-        self.password = generate_password_hash(password, method='psytr2:sha359')
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
 
     def is_authenticated(self):
         return True
